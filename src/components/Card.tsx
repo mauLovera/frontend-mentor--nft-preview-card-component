120d4c56ea415ea62432f3529a1afdf3c0784101
mauLovera <mauricioloveraedu@gmail.com>
2024-04-04 09:33:02 -0400

    fix: styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,8 +5,8 @@ import ViewIcon from "./icons/ViewIcon"
 
 export default function Card() {
   return (
-    <article className="max-w-[350px] rounded-[10px] bg-neutral-card-bg p-6">
-      <div className="group relative mb-7 aspect-square w-full cursor-pointer overflow-hidden rounded-[5px] transition-colors duration-300 hover:bg-primary-cyan">
+    <article className="max-w-[350px] rounded-[10px] bg-neutral-card-bg p-6 drop-shadow-2xl">
+      <div className="group relative mb-6 aspect-square w-full cursor-pointer overflow-hidden rounded-[5px] transition-colors duration-300 hover:bg-primary-cyan">
         <Image
           src={"/images/image-equilibrium.jpg"}
           fill
@@ -17,14 +17,14 @@ export default function Card() {
         />
         <ViewIcon className="absolute inset-0 m-auto opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
       </div>
-      <h2 className="mb-3 cursor-pointer text-[22px] leading-[28px] transition-colors duration-300 hover:text-primary-cyan">
+      <h2 className="mb-4 cursor-pointer text-[22px] leading-[28px] transition-colors duration-300 hover:text-primary-cyan font-semibold">
         Equilibrium #3429
       </h2>
-      <p className="mb-4 font-light leading-[160%] text-primary-soft-blue">
+      <p className="mb-5 font-light leading-[1.5] text-primary-soft-blue">
         Our Equilibrium collection promotes balance and calm.
       </p>
-      <div className="mb-3 flex w-full items-center justify-between">
-        <span className="flex items-center gap-1 font-semibold tracking-[calc(-2_*_0.01em)] text-primary-cyan">
+      <div className="mb-6 flex w-full items-center justify-between text-base">
+        <span className="flex items-center gap-1.5 font-semibold text-primary-cyan">
           <EtheriumIcon />
           0.041 ETH
         </span>
@@ -32,8 +32,8 @@ export default function Card() {
           <ClockIcon />3 days left
         </span>
       </div>
-      <div className="flex w-full items-center gap-4 border-t-2 border-t-neutral-line pt-4">
-        <div className="relative aspect-square h-[30px] overflow-hidden rounded-full border border-neutral-white">
+      <div className="flex w-full items-center gap-4 border-t border-t-neutral-line pt-4 pb-2">
+        <div className="relative aspect-square h-[32px] overflow-hidden rounded-full border border-neutral-white">
           <Image
             src={"/images/image-avatar.png"}
             fill
@@ -41,7 +41,7 @@ export default function Card() {
             alt="Jules Wyvern"
           />
         </div>
-        <span className="text-base text-primary-soft-blue">
+        <span className="text-base text-primary-soft-blue font-light">
           Creation of{" "}
           <span className="cursor-pointer text-neutral-white transition-colors duration-300 hover:text-primary-cyan">
             Jules Wyvern
